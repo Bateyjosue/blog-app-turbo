2c93583ea8c349778c844aa06a4e88349349b405
Bateyjosue <josuebatey19@gmail.com>
2025-05-24 14:41:34 +0200

    feat: add prisma module

---
+++ apps/api/prisma/seed.ts
@@ -0,0 +1,76 @@
+import { faker } from '@faker-js/faker/.';
+import { PrismaClient } from '@prisma/client';
+import { Post } from 'generated/prisma';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const users = Array.from({ length: 10 }).map(() => ({
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    bio: faker.lorem.sentence(),
+    avatar: faker.image.avatar(),
+  }));
+
+  await prisma.user.createMany({
+    data: users,
+  });
+
+  // seed post too
+  const posts = Array.from({ length: 10 }).map(() => ({
+    title: faker.lorem.sentence(),
+    slug: generateSlug(faker.lorem.sentence()),
+    content: faker.lorem.paragraph(3),
+    thumbnail: faker.image.urlLoremFlickr(),
+    published: true,
+    authorId: faker.number.int({ min: 1, max: 10 }),
+  }));
+
+  await Promise.all(
+    posts.map(async (post: Post) => {
+      await prisma.post.create({
+        data: {
+          ...post,
+          comments: {
+            createMany: {
+              data: Array.from({ length: 10 }).map(() => ({
+                content: faker.lorem.sentence(),
+                authorId: faker.number.int({ min: 1, max: 10 }),
+              })),
+            },
+          },
+        },
+      });
+    }),
+  );
+
+  // seed comment too
+  const comments = Array.from({ length: 10 }).map(() => ({
+    content: faker.lorem.sentence(),
+    postId: faker.number.int({ min: 1, max: 10 }),
+    authorId: faker.number.int({ min: 1, max: 10 }),
+  }));
+
+  await prisma.comment.createMany({
+    data: comments,
+  });
+}
+
+function generateSlug(title: string): string {
+  return title
+    .toLowerCase()
+    .trim()
+    .replace(/ /g, '-')
+    .replace(/[^\w-]+/g, '');
+}
+
+main()
+  .then(() => {
+    prisma.$disconnect();
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error('Error seeding database:', error);
+    prisma.$disconnect();
+    process.exit(1);
+  });
